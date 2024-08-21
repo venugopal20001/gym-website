397b9374836247d7ffff349ba4ee73afc78c25bc
venu <61859163+venugopal20001@users.noreply.github.com>
2024-08-22 01:28:10 +0530

    Update Header.jsx

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -8,7 +8,7 @@ function Header() {
             <h1><span>FITNESS</span>WITH US</h1>
             <p className='details'>Build Your Body And Fitness With Professional Touch</p>
             <div className='header-btns'>
-                <a href='#' className='header-btn'>Join Us</a>
+                <a href='main' className='header-btn'>Join Us</a>
 
             </div>
         </div>
