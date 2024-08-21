557abc2f68147a3b36dc3f1b2e0ca1f3b1f4d057
venu <61859163+venugopal20001@users.noreply.github.com>
2024-08-22 01:29:06 +0530

    Update Offer.jsx

--- src/components/Offer.jsx
+++ src/components/Offer.jsx
@@ -7,7 +7,7 @@ function Offer() {
             <h1> Big <span>OFFER</span>This summer</h1>
             <p className='details'>Lorem, ipsum dolor sit amet consectetur adipisicing.</p>
             <div className='pr-btns'>
-                <a href='#' className='pr-btn'>JOIN NOW</a>
+                <a href='presentation' className='pr-btn'>JOIN NOW</a>
             </div>
         </div>
     </div>
